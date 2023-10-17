794752d604ace03fc207c9f5444f05ff8d17e3ef
gotthebunnyinside <gotthebunnyinside@hotmail.com>
2023-10-17 10:23:03 -0500

    update

--- NewAiIdeas/Assets/Scripts/TechNode.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-[System.Serializable]
-public class TechNode
-{
-    public string techName;
-    public bool isResearched = false;
-    public List<TechNode> prerequisites;
-}
--- NewAiIdeas/Assets/Scripts/Units/UnitActions/AIAction.cs
+++
@@ -1,5 +0,0 @@
-public abstract class AIAction
-{
-    public abstract float Evaluate(AIContext context);
-    public abstract void Execute(AIContext context);
-}
